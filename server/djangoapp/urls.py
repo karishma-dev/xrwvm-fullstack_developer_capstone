9db74e233aa6a86975036980eeb9cc5f1310483e
karishma-dev <jainkarishma76@gmail.com>
2026-07-05 02:52:43 -0400

    4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('logout/', views.logout_request, name='logout'),
     path('register/', views.registration, name='register'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
